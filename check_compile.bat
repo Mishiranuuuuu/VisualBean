538ed19e21d235915ba288a71e060815a2a63e11
WakaranaiDesu <86252890+Mishiranuuuuu@users.noreply.github.com>
2026-02-05 01:11:54 +0700

    Delete check_compile.bat

--- check_compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not exist bin mkdir bin
-javac -d bin -sourcepath src src/com/vnengine/Main.java
-if %errorlevel% neq 0 (
-    echo COMPILATION FAILED
-) else (
-    echo COMPILATION SUCCESS
-)
